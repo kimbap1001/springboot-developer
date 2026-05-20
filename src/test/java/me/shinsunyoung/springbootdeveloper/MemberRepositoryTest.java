1ec346071a8faa97dca97747917f67b16343be3d
kimbap1001 <qndldhd1@naver.com>
2026-05-20 16:54:35 +0900

    test: Add MemberRepository findAll test

---
+++ src/test/java/me/shinsunyoung/springbootdeveloper/MemberRepositoryTest.java
@@ -0,0 +1,25 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Sql("/insert-members.sql")
+    @Test
+    void getAllMembers() {
+        List<Member> members = memberRepository.findAll();
+
+        assertThat(members.size()).isEqualTo(3);
+    }
+}
