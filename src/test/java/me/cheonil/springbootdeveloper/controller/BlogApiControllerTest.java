50f9a6f01047c8864aa89730606f6777b6f06ad4
kimbap1001 <qndldhd1@naver.com>
2026-05-24 16:53:03 +0900

    test: add BlogApiConcroller updateArticle test

--- src/test/java/me/cheonil/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/me/cheonil/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -3,6 +3,7 @@ package me.cheonil.springbootdeveloper.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.cheonil.springbootdeveloper.domain.Article;
 import me.cheonil.springbootdeveloper.dto.AddArticleRequest;
+import me.cheonil.springbootdeveloper.dto.UpdateArticleRequest;
 import me.cheonil.springbootdeveloper.repository.BlogRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -140,5 +141,37 @@ class BlogApiControllerTest {
         List<Article> articles = blogRepository.findAll();
 
         assertThat(articles).isEmpty();
+    }
+
+    @DisplayName("updateArticle: 블로그 글 수정에 성공한다.")
+    @Test
+    public void updateArticle() throws Exception{
+        //given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        final String newTitle = "new Title";
+        final String newContent = "new Content";
+
+        UpdateArticleRequest request = new UpdateArticleRequest(newTitle, newContent);
+
+        //when
+        ResultActions result = mockMvc.perform(put(url,savedArticle.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)));
+
+        //then
+        result.andExpect(status().isOk());
+
+        Article article = blogRepository.findById(savedArticle.getId()).get();
+
+        assertThat(article.getTitle()).isEqualTo(newTitle);
+        assertThat(article.getContent()).isEqualTo(newContent);
     }
 }
